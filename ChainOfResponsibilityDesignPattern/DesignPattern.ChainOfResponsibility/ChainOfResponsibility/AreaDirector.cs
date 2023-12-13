c9203c24ea961cb68ba4b481083a51697af108ba
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-13 22:33:26 +0300

    ChainOfResponsibility

---
+++ ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/ChainOfResponsibility/AreaDirector.cs
@@ -0,0 +1,33 @@
+﻿using DesignPattern.ChainOfResponsibility.DAL;
+using DesignPattern.ChainOfResponsibility.Models;
+
+namespace DesignPattern.ChainOfResponsibility.ChainOfResponsibility
+{
+    public class AreaDirector : Employee
+    {
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            Context context = new Context();
+            if (req.Amount <= 400000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Müdürü - Göksu Özcan";
+                customerProcess.Description = "Para Çekme İşlemi Onaylandı, Müşteriye Talep Ettiği Tutar Ödendi";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            else
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Müdürü - Göksu Özcan";
+                customerProcess.Description = "Para Çekme Tutarı Bölge Müdürünün Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Gerçekleştirilemedi Müşterinin Günlük Maksimum Çekebileceği Tutar 400.000 TL Olup Daha Fazlası İçin Birden Fazla Gün Şubeye Gelmesi Gerekli...";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+        }
+    }
+}
