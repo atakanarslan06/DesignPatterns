3522bf4d7e6cc1fde5a03b8dc3b90768079a7a75
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-14 20:49:13 +0300

    CQRS

---
+++ CQRSDesignPattern/DesignPattern.CQRS/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.CQRS.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("server=ATAKAN\\MYBLOG;initial catalog=DesignPattern2;integrated security=true;");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
