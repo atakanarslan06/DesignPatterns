7fe02952ae7dcf2f86f7ee577e72597221abc7fc
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-15 16:25:50 +0300

    ObserverDesignPattern

--- ObserverDesignPattern/DesignPattern.Observer/ObserverPattern/CreateDiscountCode.cs
+++ ObserverDesignPattern/DesignPattern.Observer/ObserverPattern/CreateDiscountCode.cs
@@ -1,6 +1,25 @@
-﻿namespace DesignPattern.Observer.ObserverPattern
+﻿using DesignPattern.Observer.DAL;
+
+namespace DesignPattern.Observer.ObserverPattern
 {
-    public class CreateDiscountCode
+    public class CreateDiscountCode : IObserver
     {
+        private readonly IServiceProvider _serviceProvider;
+        Context context = new Context();
+
+        public CreateDiscountCode(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public void CreateNewUser(AppUser appUser)
+        {
+            context.Discounts.Add(new Discount
+            {
+                DiscountCode = "DERGIMART",
+                DiscountAmount = 35,
+                DiscountCodeStatus = true,
+            });
+            context.SaveChanges();
+        }
     }
 }
