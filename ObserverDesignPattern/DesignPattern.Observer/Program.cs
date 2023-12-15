6ed980c2f770206b980cd23aa5552a47b0510e45
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-15 16:46:47 +0300

    ObserverDesignPattern

--- ObserverDesignPattern/DesignPattern.Observer/Program.cs
+++ ObserverDesignPattern/DesignPattern.Observer/Program.cs
@@ -1,6 +1,10 @@
+using DesignPattern.Observer.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
