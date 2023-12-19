a0f1797ab2ff65418d01913c518585f46d7e2e78
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-19 20:10:52 +0300

    FacadeDesignPattern

--- FacadeDesignPattern/DesignPattern.Facade/FacadePattern/OrderFacade.cs
+++ FacadeDesignPattern/DesignPattern.Facade/FacadePattern/OrderFacade.cs
@@ -1,6 +1,30 @@
-﻿namespace DesignPattern.Facade.FacadePattern
+﻿using DesignPattern.Facade.DAL;
+
+namespace DesignPattern.Facade.FacadePattern
 {
     public class OrderFacade
     {
+        Order order = new Order();
+        OrderDetail orderDetail = new OrderDetail();
+        ProductStock productStock = new ProductStock();
+
+        AddOrder addOrder = new AddOrder();
+        AddOrderDetail addOrderDetail = new AddOrderDetail();
+
+        public void CompleteOrder(int customerID, int productId, int orderID, int productCount, decimal productPrice, decimal totalProductPrice)
+        {
+            order.CustomerID = customerID;
+            addOrder.AddNewOrder(order);
+
+
+            orderDetail.CustomerID = customerID;
+            orderDetail.ProductID = productId;
+            orderDetail.ProductCount = productCount;
+            orderDetail.ProductPrice = productPrice;
+            totalProductPrice = productCount * productPrice;
+            orderDetail.ProductTotalPrice = totalProductPrice;
+            addOrderDetail.AddNewOrderDetail(orderDetail);
+        }
+
     }
 }
