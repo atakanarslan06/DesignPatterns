982c412080cde8039fc0f148ee3fd8438a4a1d4b
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-19 10:56:14 +0300

    IteratorDesignPattern

---
+++ IteratorDesignPattern/DesignPattern.Iterator/IteratorPattern/VisitRouteMover.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPattern.Iterator.IteratorPattern
+{
+    public class VisitRouteMover : IMover<VisitorRoute>
+    {
+        public List<VisitorRoute> visitorRoutes = new List<VisitorRoute> ();
+        
+        public void AddVisitRoute(VisitorRoute visitorRoute)
+        {
+            visitorRoutes.Add(visitorRoute);
+        }
+
+
+        
+        public Iterator<VisitorRoute> CreateIterator()
+        {
+            
+        }
+    }
+}
