4a4bcf7fee5ebe2fecd658a86e6c6e6cfc336aa6
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-20 10:02:02 +0300

    DecaratorDesignPattern

---
+++ DecoratorDesignPattern/DesignPattern.Decorator/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Decorator.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=ATAKAN\\MYBLOG;initial catalog=DesignPattern10;integrated security=true;");
+        }
+       // public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
