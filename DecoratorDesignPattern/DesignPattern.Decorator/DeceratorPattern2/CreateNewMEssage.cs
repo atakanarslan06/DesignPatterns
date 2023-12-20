07aeb7219f2693b572be6a2dca4240f9d8a7f26f
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-20 10:43:10 +0300

    DeceratorDesignPattern

---
+++ DecoratorDesignPattern/DesignPattern.Decorator/DeceratorPattern2/CreateNewMEssage.cs
@@ -0,0 +1,14 @@
+﻿using DesignPattern.Decorator.DAL;
+
+namespace DesignPattern.Decorator.DeceratorPattern2
+{
+    public class CreateNewMEssage : ISendMessage
+    {
+        Context context;
+        public void SendMessage(Message message)
+        {
+            context.Messages.Add(message);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DecoratorDesignPattern/DesignPattern.Decorator/DeceratorPattern2/ISendMessage.cs
@@ -0,0 +1,9 @@
+﻿using DesignPattern.Decorator.DAL;
+
+namespace DesignPattern.Decorator.DeceratorPattern2
+{
+    public class ISendMessage
+    {
+        void SendMessage(Message message);
+    }
+}
