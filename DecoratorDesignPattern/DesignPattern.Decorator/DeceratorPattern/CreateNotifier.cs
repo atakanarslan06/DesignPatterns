db4a77f7f7b8e525c2f5f2ff03514ddc7efd6d8a
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-20 10:10:15 +0300

    DecoratorDesignPattern

---
+++ DecoratorDesignPattern/DesignPattern.Decorator/DeceratorPattern/CreateNotifier.cs
@@ -0,0 +1,14 @@
+﻿using DesignPattern.Decorator.DAL;
+
+namespace DesignPattern.Decorator.DeceratorPattern
+{
+    public class CreateNotifier : INotifier
+    {
+        Context context;
+        public void CreateNotify(Notifier notifier)
+        {
+            context.Notifiers.Add(notifier);
+            context.SaveChanges
+        }
+    }
+}
